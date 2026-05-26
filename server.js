1f6dbd7c345429e99f53a97a9b800e93428aa673
jessetusiime <jessetusiime078@gmail.com>
2026-05-26 21:52:00 +0300

    Fixed local rendering issue

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
-
+import session from 'express-session';
+import flash from './src/middleware/flash.js';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
@@ -18,9 +20,16 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-/**
-  * Configure Express middleware
-  */
+
+// Set up session management
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+}));
+
+app.use(flash);
 
 // Allow Express to receive and process common POST data
 app.use(express.urlencoded({ extended: true }));
